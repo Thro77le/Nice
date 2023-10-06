e7f1e350c18091c186c3d0cae8d1153cc3ab6773
Thro77le <throt7le@gmail.com>
2023-10-06 13:11:42 +0200

    1.0

---
+++ src/Pretty.sol
@@ -0,0 +1,120 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.18;
+
+import { Strings } from "@openzeppelin/contracts/utils/Strings.sol";
+
+/// @author Throttle
+/// @title Pretty number formatting library
+/// @notice A library that returns number in decimal representation
+library Pretty {
+    using Strings for uint256;
+    using Strings for int256;
+
+    bytes1 constant ZERO = abi.encodePacked("0")[0];
+    bytes1 constant DOT = abi.encodePacked(".")[0];
+    bytes1 constant SPACE = abi.encodePacked(" ")[0];
+    bytes1 constant MINUS = abi.encodePacked("-")[0];
+
+    /// @param number Number to be formatted
+    /// @return string representation of a number
+    function format(uint256 number) internal pure returns (string memory) {
+        return _format(number, 18);
+    }
+
+    /// @param number Number to be formatted
+    /// @param decimals Number of decimal places
+    /// @return string representation of a number
+    function format(uint256 number, uint8 decimals) internal pure returns (string memory) {
+        return _format(number, decimals);
+    }
+
+    /// @param number Number to be formatted
+    /// @return string representation of a number
+    function format(int256 number) internal pure returns (string memory) {
+        return _format(number, 18);
+    }
+
+    /// @param number Number to be formatted
+    /// @param decimals Number of decimal places
+    /// @return string representation of a number
+    function format(int256 number, uint8 decimals) internal pure returns (string memory) {
+        return _format(number, decimals);
+    }
+
+    function _format(uint256 number, uint8 decimals) private pure returns (string memory) {
+        if (number == 0) {
+            return "0";
+        }
+        uint256 base = 10 ** decimals;
+        bytes memory s;
+
+        if (number < base) {
+            uint256 number_plus_10 = number + 10 * base;
+            s = abi.encodePacked(number_plus_10.toString());
+
+            s[0] = ZERO;
+            s[1] = DOT;
+        } else {
+            uint256 decimalPart = number % base;
+            uint256 integerPart = (number - decimalPart) * 10; // move integer part 1 to the left for the dot
+            s = abi.encodePacked((integerPart + decimalPart).toString());
+
+            uint256 dot_index = s.length - decimals - 1;
+            s[dot_index] = DOT;
+        }
+
+        return string(cut_trailing_zeros(s, decimals));
+    }
+
+    function _format(int256 _number, uint8 decimals) private pure returns (string memory) {
+        int256 number = _number;
+        if (number == int256(0)) {
+            return "0";
+        }
+
+        if (number > int256(0)) {
+            return _format(uint256(_number), decimals);
+        }
+
+        int256 base = -int256(10 ** decimals);
+        bytes memory s;
+
+        if (base < number) {
+            int256 number_minus_10 = number + 10 * base;
+            s = abi.encodePacked(number_minus_10.toString());
+
+            s[1] = ZERO;
+            s[2] = DOT;
+        } else {
+            int256 decimalPart = number % base;
+            int256 integerPart = (number - decimalPart) * 10; // move integer part 1 to the left for the dot
+            s = abi.encodePacked((integerPart + decimalPart).toString());
+
+            uint256 dot_index = s.length - decimals - 1;
+            s[dot_index] = DOT;
+        }
+
+        return string(cut_trailing_zeros(s, decimals));
+    }
+
+    function cut_trailing_zeros(bytes memory s, uint8 decimals) private pure returns (bytes memory) {
+        uint256 count = 0;
+        for (uint256 i = s.length - 1; s[i] == ZERO; i--) {
+            count++;
+        }
+
+        if (count == 0) {
+            return s;
+        }
+
+        if (count == decimals) {
+            count--;
+        }
+
+        bytes memory new_s = new bytes(s.length - count);
+        for (uint256 i = 0; i < new_s.length; ++i) {
+            new_s[i] = s[i];
+        }
+        return new_s;
+    }
+}
---
+++ test/Pretty.t.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {Pretty} from "../src/Pretty.sol";
+
+contract PrettyTest is Test {
+  using Pretty for uint256;
+  using Pretty for int256;
+
+  function setUp() public {}
+
+  function test_uint256() public {
+    uint256 x = 1.1e18;
+    assertEq(x.format(), "1.1");
+
+    x = 0.1e18;
+    assertEq(x.format(), "0.1");
+
+    x = 1.0e18;
+    assertEq(x.format(), "1.0");
+
+    x = 1.1234567891234567e18;
+    assertEq(x.format(), "1.1234567891234567");
+
+    x = 1.01e18;
+    assertEq(x.format(), "1.01");
+
+    x = 1.123456789123e18;
+    assertEq(x.format(), "1.123456789123");
+
+    x = 1.123000789000e18;
+    assertEq(x.format(), "1.123000789");
+
+    x = 1_234_567.123456789123e18;
+    assertEq(x.format(), "1234567.123456789123");
+  }
+
+  function test_uint256_custom_decimals() public {
+    uint256 x = 1.1e30;
+    assertEq(x.format(30), "1.1");
+  }
+
+  function test_int256() public {
+    int256 x = -1.1e18;
+    assertEq(x.format(), "-1.1");
+
+    x = -0.1e18;
+    assertEq(x.format(), "-0.1");
+
+    x = -1.0e18;
+    assertEq(x.format(), "-1.0");
+  }
+
+  function test_int256_custom_decimals() public {
+    int256 x = -1.1e30;
+    assertEq(x.format(30), "-1.1");
+  }
+}
