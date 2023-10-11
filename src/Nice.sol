82daf5ab8695e892f28671e52798757a87264538
Thro77le <throt7le@gmail.com>
2023-10-11 15:49:28 +0200

    Rewrite. Get rid of OZ lib

--- src/Nice.sol
+++ src/Nice.sol
@@ -1,120 +1,123 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.18;
 
-import { Strings } from "@openzeppelin/contracts/utils/Strings.sol";
-
 /// @author Throttle
 /// @title Nice number formatting library
 /// @notice A library that returns number in decimal representation
 library Nice {
-    using Strings for uint256;
-    using Strings for int256;
+  bytes16 private constant _SYMBOLS = "0123456789";
 
-    bytes1 constant ZERO = abi.encodePacked("0")[0];
-    bytes1 constant DOT = abi.encodePacked(".")[0];
-    bytes1 constant SPACE = abi.encodePacked(" ")[0];
-    bytes1 constant MINUS = abi.encodePacked("-")[0];
+  /// @param number Number to be formatted
+  /// @return string representation of a number
+  function format(uint256 number) internal pure returns (string memory) {
+    return _format(number, 1e18, false);
+  }
 
-    /// @param number Number to be formatted
-    /// @return string representation of a number
-    function format(uint256 number) internal pure returns (string memory) {
-        return _format(number, 18);
+  /// @param number Number to be formatted
+  /// @param decimals Number of decimal places
+  /// @return string representation of a number
+  function format(uint256 number, uint8 decimals) internal pure returns (string memory) {
+    uint256 base = 10 ** decimals;
+    return _format(number, base, false);
+  }
+
+  /// @param number Number to be formatted
+  /// @return string representation of a number
+  function format(int256 number) internal pure returns (string memory) {
+    if (number < 0) {
+      return _format(uint256(-number), 1e18, true);
+    } else {
+      return _format(uint256(number), 1e18, false);
     }
+  }
 
-    /// @param number Number to be formatted
-    /// @param decimals Number of decimal places
-    /// @return string representation of a number
-    function format(uint256 number, uint8 decimals) internal pure returns (string memory) {
-        return _format(number, decimals);
+  /// @param number Number to be formatted
+  /// @param decimals Number of decimal places
+  /// @return string representation of a number
+  function format(int256 number, uint8 decimals) internal pure returns (string memory) {
+    uint256 base = 10 ** decimals;
+    if (number < 0) {
+      return _format(uint256(-number), base, true);
+    } else {
+      return _format(uint256(number), base, false);
     }
+  }
 
-    /// @param number Number to be formatted
-    /// @return string representation of a number
-    function format(int256 number) internal pure returns (string memory) {
-        return _format(number, 18);
+  function _format(uint256 number, uint256 base, bool negative) private pure returns (string memory) {
+    uint256 integer = number / base;
+    uint256 fraction = number % base;
+    uint256 fraction_length = _count_fraction(fraction, base);
+    uint256 integer_length = _count_integer(integer);
+
+    unchecked {
+      if (negative) {
+        integer_length++;
+      }
+      uint256 length = integer_length + fraction_length + 1;
+      uint256 ptr = length;
+
+      bytes memory buffer = new bytes(length);
+      for (uint256 i = 0; i < length; i++) {
+        buffer[i] = "0";
+      }
+      if (negative) {
+        buffer[0] = "-";
+      }
+
+      // fraction part
+      if (fraction != 0) {
+        while (fraction % 10 == 0) {
+          fraction /= 10;
+        }
+        while (fraction != 0) {
+          buffer[--ptr] = _SYMBOLS[fraction % 10];
+          fraction /= 10;
+        }
+      }
+
+      buffer[integer_length] = ".";
+
+      // integer part
+      ptr = integer_length;
+      while (integer != 0) {
+        buffer[--ptr] = _SYMBOLS[integer % 10];
+        integer /= 10;
+      }
+      return string(buffer);
     }
+  }
 
-    /// @param number Number to be formatted
-    /// @param decimals Number of decimal places
-    /// @return string representation of a number
-    function format(int256 number, uint8 decimals) internal pure returns (string memory) {
-        return _format(number, decimals);
+  function _count_fraction(uint256 _number, uint256 base) private pure returns (uint256 counter) {
+    unchecked {
+      if (_number == 0) {
+        return 1;
+      }
+
+      uint256 number = _number + base;
+
+      while (number % 10 == 0) {
+        number /= 10;
+      }
+      while (number != 0) {
+        counter++;
+        number /= 10;
+      }
+
+      // subtract spot for the base unit
+      counter--;
     }
+  }
 
-    function _format(uint256 number, uint8 decimals) private pure returns (string memory) {
-        if (number == 0) {
-            return "0";
-        }
-        uint256 base = 10 ** decimals;
-        bytes memory s;
-
-        if (number < base) {
-            uint256 number_plus_10 = number + 10 * base;
-            s = abi.encodePacked(number_plus_10.toString());
-
-            s[0] = ZERO;
-            s[1] = DOT;
-        } else {
-            uint256 decimalPart = number % base;
-            uint256 integerPart = (number - decimalPart) * 10; // move integer part 1 to the left for the dot
-            s = abi.encodePacked((integerPart + decimalPart).toString());
-
-            uint256 dot_index = s.length - decimals - 1;
-            s[dot_index] = DOT;
-        }
-
-        return string(cut_trailing_zeros(s, decimals));
+  function _count_integer(uint256 _number) private pure returns (uint256 counter) {
+    uint256 number = _number;
+    if (number == 0) {
+      return 1;
     }
-
-    function _format(int256 _number, uint8 decimals) private pure returns (string memory) {
-        int256 number = _number;
-        if (number == int256(0)) {
-            return "0";
-        }
-
-        if (number > int256(0)) {
-            return _format(uint256(_number), decimals);
-        }
-
-        int256 base = -int256(10 ** decimals);
-        bytes memory s;
-
-        if (base < number) {
-            int256 number_minus_10 = number + 10 * base;
-            s = abi.encodePacked(number_minus_10.toString());
-
-            s[1] = ZERO;
-            s[2] = DOT;
-        } else {
-            int256 decimalPart = number % base;
-            int256 integerPart = (number - decimalPart) * 10; // move integer part 1 to the left for the dot
-            s = abi.encodePacked((integerPart + decimalPart).toString());
-
-            uint256 dot_index = s.length - decimals - 1;
-            s[dot_index] = DOT;
-        }
-
-        return string(cut_trailing_zeros(s, decimals));
+    unchecked {
+      while (number != 0) {
+        counter++;
+        number /= 10;
+      }
     }
-
-    function cut_trailing_zeros(bytes memory s, uint8 decimals) private pure returns (bytes memory) {
-        uint256 count = 0;
-        for (uint256 i = s.length - 1; s[i] == ZERO; i--) {
-            count++;
-        }
-
-        if (count == 0) {
-            return s;
-        }
-
-        if (count == decimals) {
-            count--;
-        }
-
-        bytes memory new_s = new bytes(s.length - count);
-        for (uint256 i = 0; i < new_s.length; ++i) {
-            new_s[i] = s[i];
-        }
-        return new_s;
-    }
+  }
 }
